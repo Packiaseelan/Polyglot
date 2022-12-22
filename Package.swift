dba2897cf89be73fdf2348e0c5bf7a28aad7ef32
Packiaseelan S <packiaseelan14@gmail.com>
2022-12-22 16:58:40 +0530

    init

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let name = "Polyglot"
+let version = "V1.0.0"
+
+let package = Package(
+    name: "Polyglot",
+    products: [
+        .library(name: name, targets: [name]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: name,
+            url: "https://github.com/Packiaseelan/Polyglot/releases/download/\(version)/\(name).xcframework.zip",
+            checksum: "c2008fb454d82876a20a707a583cf2c0bedd2403d3c41d2f1c08680cc1de9092"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Polyglot
+
+A description of this package.
