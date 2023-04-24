7e8dbd9a82f440624858791c90cd2b8a44bc6f6a
Packiaseelan S <packiaseelan14@gmail.com>
2023-04-24 22:49:57 +0530

    a

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: name,
             url: "https://github.com/Packiaseelan/Polyglot/releases/download/\(version)/\(name).xcframework.zip",
-            checksum: "cb25563900ccac2eefd6fb1cf693a8a83c0f45760d3dc6c16f04d41ad0453d45"
+            checksum: "a3668bc13f179f0046dbd1773ba256770aa3944ae8d39c1a456cc9520bb61636"
         ),
 //        .binaryTarget(name: name, path: "../../../output/\(name).xcframework")
     ]
